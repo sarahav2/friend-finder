781dc039a5c649dffbb771a96ff27f583010c6f5
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-18 19:33:38 -0500

    made more changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,5 @@
-// Dependencies
-// =============================================================
-var path = require("path");
+// Pull in required dependencies
+var path = require('path');
 
 // Import the list of friend entries
 var friends = require('../data/friends.js');
